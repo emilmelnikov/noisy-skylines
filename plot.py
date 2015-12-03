db34e0b8f3096ea63ed5a346aab353f586a9808b
Emil Melnikov <emilmelnikov@gmail.com>
2015-12-03 12:49:17 +0300

    Change interface of plot.py

--- plot.py
+++ plot.py
@@ -13,28 +13,27 @@ import matplotlib.pyplot as plt
 def main():
     ap = argparse.ArgumentParser(description=__doc__)
     ap.add_argument(
-        '-d',
-        '--dataset',
+        'dataset',
         type=str,
-        help='name of the file with the entire dataset; default is "dataset.csv"',
-        default='dataset.csv',
+        help='name of the file with the dataset',
     )
     ap.add_argument(
-        '-s',
-        '--skyline',
+        'skyline',
         type=str,
-        help='name of the file with the skyline indices; default is "skyline.csv"',
-        default='skyline.csv',
+        help='name of the file with the skyline indices',
     )
     args = ap.parse_args()
-    dataset = np.loadtxt(args.dataset, dtype=int, ndmin=2)
+    dataset = np.loadtxt(args.dataset, dtype=float, delimiter=',', ndmin=2)
     skyline = np.loadtxt(args.skyline, dtype=int)
     if len(skyline):
-        format_string = '{!s:>10} {!s:>10} {!s:>10} {!s:>10}'
-        print(format_string.format('index', 'item_index', 'x', 'y'))
-        print('-' * 43)
+        ruler_len = 73
+        format_string = '{!s:>15} {!s:>15} {!s:>20} {!s:>20}'
+        print('=' * ruler_len)
+        print(format_string.format('#', 'item_index', 'x', 'y'))
+        print('-' * ruler_len)
         for index, item_index in enumerate(skyline, 1):
             print(format_string.format(index, item_index, dataset[item_index,0], dataset[item_index,1]))
+        print('=' * ruler_len)
     if len(dataset):
         plt.scatter(dataset[:,0], dataset[:,1], color='blue', marker='.')
     if len(skyline):
